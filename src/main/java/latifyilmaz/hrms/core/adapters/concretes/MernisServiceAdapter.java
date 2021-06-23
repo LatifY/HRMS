9f6f3e1505303a899ce158d850cd26445f83adc1
Abdullatif YILMAZ <latifyilmaza@gmail.com>
2021-06-24 01:12:21 +0300

    mernis deactivated

--- src/main/java/latifyilmaz/hrms/core/adapters/concretes/MernisServiceAdapter.java
+++ src/main/java/latifyilmaz/hrms/core/adapters/concretes/MernisServiceAdapter.java
@@ -1,7 +1,7 @@
 package latifyilmaz.hrms.core.adapters.concretes;
 
 import latifyilmaz.hrms.core.adapters.abstracts.UserCheckService;
-import latifyilmaz.hrms.services.MernisService.MAMKPSPublicSoap;
+//import latifyilmaz.hrms.services.MernisService.MAMKPSPublicSoap;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -9,10 +9,11 @@ import java.util.Date;
 @Component
 public class MernisServiceAdapter implements UserCheckService {
     public boolean isRealPerson(String identityNo, String firstName, String lastName, int birthYear) {
-        MAMKPSPublicSoap soapClient = new MAMKPSPublicSoap();
+        //MAMKPSPublicSoap soapClient = new MAMKPSPublicSoap();
         boolean checkResult = false;
         try{
-            checkResult = soapClient.TCKimlikNoDogrula(Long.parseLong(identityNo), firstName, lastName, birthYear);
+           //checkResult = soapClient.TCKimlikNoDogrula(Long.parseLong(identityNo), firstName, lastName, birthYear);
+            checkResult = true;
         }
         catch (Exception e){
             System.out.println("Mernis Error");
