abdfa269b346778efeb16c0d8bb028d42f72f477
Abdullatif YILMAZ <latifyilmaza@gmail.com>
2021-06-24 00:55:44 +0300

    mernis updated

--- src/main/java/latifyilmaz/hrms/core/adapters/concretes/MernisServiceAdapter.java
+++ src/main/java/latifyilmaz/hrms/core/adapters/concretes/MernisServiceAdapter.java
@@ -1,7 +1,7 @@
 package latifyilmaz.hrms.core.adapters.concretes;
 
 import latifyilmaz.hrms.core.adapters.abstracts.UserCheckService;
-import latifyilmaz.hrms.services.MernisService.OGJKPSPublicSoap;
+import latifyilmaz.hrms.services.MernisService.MAMKPSPublicSoap;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -9,7 +9,7 @@ import java.util.Date;
 @Component
 public class MernisServiceAdapter implements UserCheckService {
     public boolean isRealPerson(String identityNo, String firstName, String lastName, int birthYear) {
-        OGJKPSPublicSoap soapClient = new OGJKPSPublicSoap();
+        MAMKPSPublicSoap soapClient = new MAMKPSPublicSoap();
         boolean checkResult = false;
         try{
             checkResult = soapClient.TCKimlikNoDogrula(Long.parseLong(identityNo), firstName, lastName, birthYear);
