bf4beb75d5adce193ce9bcbf9fc52d99d88d0d74
LatifY <latifyilmaza@gmail.com>
2021-06-29 16:22:09 +0300

    update query changed

--- src/main/java/latifyilmaz/hrms/dataAccess/abstracts/AbilityDao.java
+++ src/main/java/latifyilmaz/hrms/dataAccess/abstracts/AbilityDao.java
@@ -4,13 +4,15 @@ import latifyilmaz.hrms.entities.concretes.Ability;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface AbilityDao extends JpaRepository<Ability, Integer> {
     List<Ability> getAllByResume_Id(int resumeId);
 
+    @Transactional
     @Modifying
-    @Query("update Ability a set a.abilityName = ?2 where a.id = ?1")
+    @Query("update Ability a set a.abilityName=:abilityName where a.id=:id")
     void updateById(int id, String abilityName);
 }
